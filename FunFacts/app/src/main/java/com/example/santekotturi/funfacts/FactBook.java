3074af260aed6f5b1e987138dc7b168ca035a1d3
skotturi <skotturi@ucsd.edu>
2014-09-29 18:09:46 -0700

    fixed project import

---
+++ FunFacts/app/src/main/java/com/example/santekotturi/funfacts/FactBook.java
@@ -0,0 +1,31 @@
+package com.example.santekotturi.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by santekotturi on 9/19/14.
+ */
+public class FactBook {
+    public String[] mFacts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
+    public String getFact(){
+
+        String fact = "hi";
+        // randomly select a fact
+        Random randomGenerated = new Random();
+        int randomNumber = randomGenerated.nextInt(mFacts.length);
+
+        fact = mFacts[randomNumber];
+
+        return fact;
+    };
+}
---
+++ FunFacts/app/src/main/java/com/example/santekotturi/funfacts/FunFactsActivity.java
@@ -0,0 +1,55 @@
+package com.example.santekotturi.funfacts;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+
+public class FunFactsActivity extends Activity {
+
+    private static final String TAG = FunFactsActivity.class.getSimpleName();
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our variables
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+
+                // update text
+                factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+//        Toast.makeText(this, "Yay! Everything in MainActivity Loaded", Toast.LENGTH_SHORT).show();
+        Log.d(TAG ,"we're logging from oncreate message" );
+    }
+
+}
+
